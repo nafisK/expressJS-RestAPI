967a328c5f63040825cc74601b38b4ba8db6af2a
Nafis Khan <49691160+nafisK@users.noreply.github.com>
2022-06-19 18:45:03 -0400

    added courses example and get methods

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
 // Returns Msg to listener
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send(`Course with id ${req.params.id} not found.`);
+  res.send(course);
 });
 
 // using environment saved in global process object or 3000
